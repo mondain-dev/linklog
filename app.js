efe9b5deaee1574c018b77f917bb09c4e44794dc
mondain-dev <mondain.sciamhaevensis@gmail.com>
2021-11-23 01:00:24 +1100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,76 @@ var RSSParser = require('rss-parser');
 let rssParser = new RSSParser();
 var RSS = require('rss');
 
-var feedConfig = toml.parse(fs.readFileSync('feeds.toml', 'utf8')).feeds;
+var parseURL = require("whatwg-url").parseURL;
+var getLinkPreview = require("link-preview-js").getLinkPreview;
+var unfurl = require("unfurl.js").unfurl;
+
+async function getLinkContent(url){
+    let linkDescription = "";
+    try {
+        let linkPreview = await getLinkPreview(url, {timeout:5000});
+        if (linkPreview.contentType.startsWith("text/html"))
+        {
+            try{
+                let linkData = await unfurl(url, {timeout:5000});
+                try {
+                    linkDescription += "<p><img src=\"" + linkData.open_graph.images[0].url + "\"></p>";
+                }
+                catch(error){
+                    // console.log(error);
+                }
+                try{
+                    // linkDescription += linkData.oEmbed.html.replace(/<script.*>,*<\/script>/ims, " ");
+                    linkDescription += '<p>' + linkData.title+ '</p>';
+                }
+                catch (error){
+                    console.log(error);
+                }
+            }
+            catch (error){
+                console.log("unfurl(" + url + ") failed: ")
+                console.log(error);
+            }
+        }
+        else if(linkPreview.contentType.startsWith("image")){
+            linkDescription += "<img src=\"" + linkPreview.url + "\">"
+        }
+    } catch (error) {
+        console.log("getLinkPreview(" + url + ") failed: ")
+        console.log(error);
+    }
+    return linkDescription;
+}
+
+let renderTweet = getLinkContent;
+
+async function fetchTitle(url){
+    let linkTitle = "";
+    try {
+        let linkPreview = await getLinkPreview(url, {timeout:5000});
+        if (linkPreview.contentType.startsWith("text/html"))
+        {
+            try{
+                let linkData = await unfurl(url, {timeout:5000});
+                try{
+                    // linkDescription += linkData.oEmbed.html.replace(/<script.*>,*<\/script>/ims, " ");
+                    linkTitle = linkData.title;
+                }
+                catch (error){
+                    console.log(error);
+                }
+            }
+            catch (error){
+                console.log("unfurl(" + url + ") failed: ")
+                console.log(error);
+            }
+        }
+    } catch (error) {
+        console.log("getLinkPreview(" + url + ") failed: ")
+        console.log(error);
+    }
+    return linkTitle;
+}
 
 function validateURL(string) {
     try {
@@ -41,29 +110,40 @@ let extractLinks = async (entry, excludes, cssSelector = 'a') => {
     let links = [];
     if(entryContent){
         const $ = cheerio.load(entryContent);
-        $(cssSelector).each( async function () {
-            if (excludes.every((t)=>{return !$(this).attr('href').includes(t) && !$(this).text().includes(t) })){
-                let linkURL = $(this).attr('href');
+        for (let el of $(cssSelector)){
+            if (excludes.every((t)=>{return !$(el).attr('href').includes(t) && !$(el).text().includes(t) })){
+                let linkURL = $(el).attr('href');
                 if (validateURL(linkURL))
                 {
-                    let linkTitle = extractTitle($(this).html());
+                    let linkTitle = extractTitle($(el).html());
+                    if(validateURL(linkTitle)){
+                        linkTitle = fetchTitle(linkURL);
+                    }
                     if(!linkTitle){
                         linkTitle = linkURL;
                     }
-                    let linkDescription = "<p>URL: <a href=\"" + linkURL +"\">" + linkURL  + "</a></p><p>source: <a href=\"" + entry.link +"\">" + entry.title + "</a></p>";
+                    
+                    // let linkContent = 
+                    let linkContent = "<p>URL: <a href=\"" + linkURL +"\">" + linkURL  + "</a></p><p>source: <a href=\"" + entry.link +"\">" + entry.title + "</a></p>";
+                    if (/twitter.com$/.test(parseURL(linkURL).host.toLocaleLowerCase())){
+                        let tweetContent = await renderTweet(linkURL);
+                        linkContent = tweetContent + linkContent;
+                    }
                     let linkEntry = {
                         url: linkURL, 
                         title: linkTitle,
-                        description: linkDescription,
+                        description: linkContent,
                         date: entry.pubDate,
                         author: entry.author,
                     };
+
                     if( ! links.map((e)=> {return e.url}).includes(linkEntry.url) ){
                         links.push(linkEntry);
                     }
+                    //console.log(linkDescription);
                 }
-            }       
-        });
+            } 
+        }
     }
     return links;
 }
@@ -73,7 +153,7 @@ let loadFeeds = async (feedConfig) => {
     for(const f of feedConfig)
     {
         await rssParser.parseURL(f.url).then( async (feedContent) => {
-            for(let entry of feedContent.items)
+            for (let entry of feedContent.items)
             {
                 let includeEntry = true;
                 if('entry' in f){
@@ -97,10 +177,12 @@ let loadFeeds = async (feedConfig) => {
             }
         });
     }
+    // entries = await Promise.all(entries);
     return entries;
 }
 
-let outputFeed = new RSS({title:"Linklog", feed_url:"https://mondain-dev.github.io/linklog/index.xml", site_url:"https://github.com/mondain-dev/linklog"});
+let feedConfig = toml.parse(fs.readFileSync('feeds.toml', 'utf8')).feeds;
+let outputFeed = new RSS({title:"Linklog", feed_url:"https://mondain-dev.github.com/linklog/index.xml", site_url:"https://github.com/mondain-dev/linklog"});
 
 loadFeeds(feedConfig).then((items) => {
     for(let item of items){
